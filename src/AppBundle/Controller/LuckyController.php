4a5dcb7fa2b0efe1bf8fde13b5f326554b21776f
Jakub Karło <jakub.karlo@o2.pl>
2017-03-21 21:21:57 +0100

    Added bundles for french english and polish
    The problem is it doesn't work when you try to set locale

--- src/AppBundle/Controller/LuckyController.php
+++ src/AppBundle/Controller/LuckyController.php
@@ -8,23 +8,27 @@
 
 namespace AppBundle\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 
-class LuckyController
+class LuckyController extends Controller
 {
     /**
      * @Route("/lucky/number")
+     * @param Request $request
+     * @return Response
      */
 
-    public function numberAction()
-    {
-        $number  = mt_rand(0,100);
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+    public function numberAction(Request $request)
+    {
+
+        $request->setLocale('pl');
+        return $this->render('lucky/number.html.twig');
+
     }
 
 }
